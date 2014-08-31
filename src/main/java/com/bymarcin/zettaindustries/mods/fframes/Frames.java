f1e6c219aa8d659358742a4f96ac1a15dceb4d5c
marcin212 <m-d14@o2.pl>
2014-08-31 05:48:29 +0200

    LarvaeFrame

---
+++ src/main/java/com/bymarcin/zettaindustries/mods/fframes/Frames.java
@@ -0,0 +1,21 @@
+package com.bymarcin.zettaindustries.mods.fframes;
+
+import com.bymarcin.zettaindustries.modmanager.IMod;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+
+public class Frames implements IMod{
+
+	@Override
+	public void init() {
+		GameRegistry.registerItem(new LarvaeFrame(), "larvaeFrame");
+	}
+
+	@Override
+	public void postInit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
