ab36fe0bcc93bfa40d891da6b8929e9b473ee56e
marcin212 <m-d14@o2.pl>
2014-10-03 15:43:52 +0200

    more comfortable GUI battery power tap.

--- src/main/java/com/bymarcin/zettaindustries/mods/battery/gui/GuiPowerTap.java
+++ src/main/java/com/bymarcin/zettaindustries/mods/battery/gui/GuiPowerTap.java
@@ -15,8 +15,8 @@ import com.bymarcin.zettaindustries.registry.ZIRegistry;
 
 public class GuiPowerTap extends GuiContainer{
 	TileEntityPowerTap tile;
-	GuiButton plus5;
-	GuiButton minus5;
+	GuiButton plus1;
+	GuiButton minus1;
 	
 	GuiButton plus10;
 	GuiButton minus10;
@@ -26,7 +26,13 @@ public class GuiPowerTap extends GuiContainer{
 	
 	GuiButton plus1000;
 	GuiButton minus1000;
-	
+
+    GuiButton plus10000;
+    GuiButton minus10000;
+
+    GuiButton plus100000;
+    GuiButton minus100000;
+
 	private ResourceLocation guiTexture = new ResourceLocation(ZettaIndustries.MODID, "textures/gui/battery/guicube.png");
 	public final int xSize = 176;
 	public final int ySize = 88;
@@ -44,20 +50,26 @@ public class GuiPowerTap extends GuiContainer{
 		int posX = (this.width - xSize) / 2;
 		int posY = (this.height - ySize) / 2;
 							//id, x, y, width, height, text
-        plus5 = new GuiButton(1, posX+10, posY+10, 20, 20, "+5");
-        minus5 = new GuiButton(2, posX+10, posY+60, 20, 20, "-5");
+        plus1 = new GuiButton(1, posX+5, posY+10, 20, 20, "+1");
+        minus1 = new GuiButton(2, posX+5, posY+60, 20, 20, "-1");
         
-        plus10 = new GuiButton(3, posX+35, posY+10, 25, 20, "+10");
-        minus10 = new GuiButton(4, posX+35, posY+60, 25, 20, "-10");
+        plus10 = new GuiButton(3, posX+26, posY+10, 25, 20, "+10");
+        minus10 = new GuiButton(4, posX+26, posY+60, 25, 20, "-10");
         
-        plus100 = new GuiButton(5, posX+65, posY+10, 30, 20, "+100");
-        minus100 = new GuiButton(6, posX+65, posY+60, 30, 20, "-100");
+        plus100 = new GuiButton(5, posX+51, posY+10, 30, 20, "+100");
+        minus100 = new GuiButton(6, posX+51, posY+60, 30, 20, "-100");
         
-        plus1000 = new GuiButton(7, posX+100, posY+10, 40, 20, "+1000");
-        minus1000 = new GuiButton(8, posX+100, posY+60, 40, 20, "-1000");
-        
-        buttonList.add(minus5);
-        buttonList.add(plus5);
+        plus1000 = new GuiButton(7, posX+82, posY+10, 25, 20, "+1K");
+        minus1000 = new GuiButton(8, posX+82, posY+60, 25, 20, "-1K");
+
+        plus10000 = new GuiButton(9, posX+107, posY+10, 29, 20, "+10K");
+        minus10000 = new GuiButton(10, posX+107, posY+60, 29, 20, "-10K");
+
+        plus100000 = new GuiButton(11, posX+136, posY+10, 36, 20, "+100K");
+        minus100000 = new GuiButton(12, posX+136, posY+60, 36, 20, "-100K");
+
+        buttonList.add(minus1);
+        buttonList.add(plus1);
         
         buttonList.add(minus10);
         buttonList.add(plus10);
@@ -67,7 +79,13 @@ public class GuiPowerTap extends GuiContainer{
         
         buttonList.add(minus1000);
         buttonList.add(plus1000);
-        
+
+        buttonList.add(plus10000);
+        buttonList.add(minus10000);
+
+        buttonList.add(plus100000);
+        buttonList.add(minus100000);
+
 		super.initGui();
 	}
 
@@ -88,8 +106,8 @@ public class GuiPowerTap extends GuiContainer{
 	@Override
 	protected void actionPerformed(GuiButton button) {
 		switch(button.id){
-		case 1:ZIRegistry.packetHandler.sendToServer(new PowerTapUpdatePacket(tile, tile.getTransferCurrent()+5,false)); break;
-		case 2:ZIRegistry.packetHandler.sendToServer(new PowerTapUpdatePacket(tile, tile.getTransferCurrent()-5,false)); break;
+		case 1:ZIRegistry.packetHandler.sendToServer(new PowerTapUpdatePacket(tile, tile.getTransferCurrent()+1,false)); break;
+		case 2:ZIRegistry.packetHandler.sendToServer(new PowerTapUpdatePacket(tile, tile.getTransferCurrent()-1,false)); break;
 		
 		case 3:ZIRegistry.packetHandler.sendToServer(new PowerTapUpdatePacket(tile, tile.getTransferCurrent()+10,false)); break;
 		case 4:ZIRegistry.packetHandler.sendToServer(new PowerTapUpdatePacket(tile, tile.getTransferCurrent()-10,false)); break;
@@ -98,8 +116,15 @@ public class GuiPowerTap extends GuiContainer{
 		case 6:ZIRegistry.packetHandler.sendToServer(new PowerTapUpdatePacket(tile, tile.getTransferCurrent()-100,false)); break;	
 	
 		case 7:ZIRegistry.packetHandler.sendToServer(new PowerTapUpdatePacket(tile, tile.getTransferCurrent()+1000,false)); break;
-		case 8:ZIRegistry.packetHandler.sendToServer(new PowerTapUpdatePacket(tile, tile.getTransferCurrent()-1000,false)); break;		
-		}
+		case 8:ZIRegistry.packetHandler.sendToServer(new PowerTapUpdatePacket(tile, tile.getTransferCurrent()-1000,false)); break;
+
+        case 9:ZIRegistry.packetHandler.sendToServer(new PowerTapUpdatePacket(tile, tile.getTransferCurrent()+10000,false)); break;
+        case 10:ZIRegistry.packetHandler.sendToServer(new PowerTapUpdatePacket(tile, tile.getTransferCurrent()-10000,false)); break;
+
+        case 11:ZIRegistry.packetHandler.sendToServer(new PowerTapUpdatePacket(tile, tile.getTransferCurrent()+100000,false)); break;
+        case 12:ZIRegistry.packetHandler.sendToServer(new PowerTapUpdatePacket(tile, tile.getTransferCurrent()-100000,false)); break;
+
+        }
 		
 		super.actionPerformed(button);
 	}
