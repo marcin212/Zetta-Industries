2010a7aa43583f489b6a3e4bfc7be69e2605562d
marcin212 <m-d14@o2.pl>
2015-02-19 01:51:42 +0100

    added recipe for catalogue

--- src/main/java/com/bymarcin/zettaindustries/mods/ecatalogue/ECatalogueMod.java
+++ src/main/java/com/bymarcin/zettaindustries/mods/ecatalogue/ECatalogueMod.java
@@ -3,6 +3,9 @@ package com.bymarcin.zettaindustries.mods.ecatalogue;
 import com.bymarcin.zettaindustries.modmanager.IMod;
 
 import li.cil.oc.api.Driver;
+import li.cil.oc.api.Items;
+
+import net.minecraft.item.ItemStack;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 
@@ -18,8 +21,10 @@ public class ECatalogueMod implements IMod{
 
 	@Override
 	public void postInit() {
-
-		
+	    ItemStack cpu1= Items.get("cpu1").createItemStack(1);
+	    ItemStack iron= new ItemStack(net.minecraft.init.Items.iron_ingot,1);
+		ItemStack book = new ItemStack(net.minecraft.init.Items.book,1);
+		GameRegistry.addRecipe(new ItemStack(ecatalogueBlock), "IBI","BCB","IBI",'I', iron, 'C', cpu1, 'B', book)	;
 	}
 
 }
