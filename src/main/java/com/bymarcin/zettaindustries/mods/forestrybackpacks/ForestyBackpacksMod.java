dd7ef551ce6bdc04929d9ea83f43d9bde1e543b1
marcin212 <m-d14@o2.pl>
2015-07-27 18:00:44 +0200

    fix #21

--- src/main/java/com/bymarcin/zettaindustries/mods/forestrybackpacks/ForestyBackpacksMod.java
+++ src/main/java/com/bymarcin/zettaindustries/mods/forestrybackpacks/ForestyBackpacksMod.java
@@ -20,62 +20,64 @@ import forestry.api.storage.EnumBackpackType;
 import forestry.core.config.ForestryItem;
 import forestry.core.fluids.Fluids;
 
-public class ForestyBackpacksMod implements IMod{
-	
+public class ForestyBackpacksMod implements IMod {
+
 	static Item creativeBackpackT1;
-	static Item creativeBackpackT2; 
-	
+	static Item creativeBackpackT2;
+
 	static Item immersiveEngineeringBackpackT1;
 	static Item immersiveEngineeringBackpackT2;
-	
+
 	static Item OCBackpackT1;
 	static Item OCBackpackT2;
 
 	public static ItemStack oc = null;
 	public static ItemStack ie = null;
-	
+
 	@Override
 	public void preInit() {
-		creativeBackpackT1 = addBackpack(new CreativeBackpack(EnumBackpackType.T1), EnumBackpackType.T1);
-		creativeBackpackT2 = addBackpack(new CreativeBackpack(EnumBackpackType.T2), EnumBackpackType.T2);
-		
-		immersiveEngineeringBackpackT1 = addBackpack(new ImmersiveEngineeringBackpack(EnumBackpackType.T1), EnumBackpackType.T1);
-		immersiveEngineeringBackpackT2 = addBackpack(new ImmersiveEngineeringBackpack(EnumBackpackType.T2), EnumBackpackType.T2);
-		
-		OCBackpackT1 = addBackpack(new OCBackpack(EnumBackpackType.T1), EnumBackpackType.T1);
-		OCBackpackT2 = addBackpack(new OCBackpack(EnumBackpackType.T2), EnumBackpackType.T2);
+		if (BackpackManager.backpackInterface == null) return;
+			creativeBackpackT1 = addBackpack(new CreativeBackpack(EnumBackpackType.T1), EnumBackpackType.T1);
+			creativeBackpackT2 = addBackpack(new CreativeBackpack(EnumBackpackType.T2), EnumBackpackType.T2);
+
+			immersiveEngineeringBackpackT1 = addBackpack(new ImmersiveEngineeringBackpack(EnumBackpackType.T1), EnumBackpackType.T1);
+			immersiveEngineeringBackpackT2 = addBackpack(new ImmersiveEngineeringBackpack(EnumBackpackType.T2), EnumBackpackType.T2);
+
+			OCBackpackT1 = addBackpack(new OCBackpack(EnumBackpackType.T1), EnumBackpackType.T1);
+			OCBackpackT2 = addBackpack(new OCBackpack(EnumBackpackType.T2), EnumBackpackType.T2);
 	}
 
 	@Override
 	public void init() {
-		
+
 	}
 
 	@Override
 	public void postInit() {
+		if (BackpackManager.backpackInterface == null) return;
 		ie = GameRegistry.findItemStack("ImmersiveEngineering", "material", 1);
 		oc = GameRegistry.findItemStack("OpenComputers", "item", 1);
-		
-		if(ie!=null){
+
+		if (ie != null) {
 			ie.setItemDamage(4);
 			addRecipe(immersiveEngineeringBackpackT1, immersiveEngineeringBackpackT2, ie);
 		}
-		
-		if(oc!=null){
+
+		if (oc != null) {
 			oc.setItemDamage(96);
 			addRecipe(OCBackpackT1, OCBackpackT2, oc);
 		}
-		
+
 	}
-	
-	private Item addBackpack(BasicBackpack backpack, EnumBackpackType type){
+
+	private Item addBackpack(BasicBackpack backpack, EnumBackpackType type) {
 		Item backpakItem = BackpackManager.backpackInterface.addBackpack(backpack, type);
-		GameRegistry.registerItem(backpakItem,backpack.getKey());
+		GameRegistry.registerItem(backpakItem, backpack.getKey());
 		backpakItem.setCreativeTab(ZettaIndustries.instance.tabZettaIndustries);
 		return backpakItem;
 	}
-	
-	private void addRecipe(Item backpackT1, Item backpackT2, ItemStack crafting){
+
+	private void addRecipe(Item backpackT1, Item backpackT2, ItemStack crafting) {
 		RecipeManagers.carpenterManager.addRecipe(200, Fluids.WATER.getFluid(1000), null, new ItemStack(backpackT2), "WXW", "WTW", "WWW", 'X', Items.diamond, 'W',
 				ForestryItem.craftingMaterial.getItemStack(1, 3), 'T', backpackT1);
 		GameRegistry.addShapedRecipe(new ItemStack(backpackT1), "X#X", "VYV", "X#X", '#', Blocks.wool,
