6ffebaeeb7d48d1ca4b8a4409213462d28c854b7
marcin212 <m-d14@o2.pl>
2015-12-31 23:34:45 +0100

    Update to new OC

--- src/main/java/com/bymarcin/zettaindustries/mods/ecatalogue/ECatalogueMod.java
+++ src/main/java/com/bymarcin/zettaindustries/mods/ecatalogue/ECatalogueMod.java
@@ -4,6 +4,7 @@ import com.bymarcin.zettaindustries.modmanager.IMod;
 
 import li.cil.oc.api.Driver;
 import li.cil.oc.api.Items;
+import li.cil.oc.api.driver.Item;
 
 import net.minecraft.item.ItemStack;
 
@@ -28,7 +29,7 @@ public class ECatalogueMod implements IMod{
 		GameRegistry.registerTileEntity(ECatalogueTileEntity.class, "ecataloguetileentity");
 		Driver.add(new TradeStationConventer());
 		GameRegistry.registerItem(mailmanItem, "mailmanitem");
-		Driver.add(mailmanItem);
+		Driver.add((Item)mailmanItem);
 	}
 
 	@Override
