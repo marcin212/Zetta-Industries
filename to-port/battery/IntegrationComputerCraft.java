03a91a7d8343ad8f7aeb66806cae529533420298
marcin212 <marcin212@protonmail.com>
2016-07-27 18:37:05 +0200

    removed old battery files

--- to-port/battery/IntegrationComputerCraft.java
+++
@@ -1,9 +0,0 @@
-package com.bymarcin.zettaindustries.mods.battery;
-
-import com.bymarcin.zettaindustries.mods.battery.tileentity.TileEntityComputerPort;
-
-public class IntegrationComputerCraft {
-	public static void computercraftInit(){
-		dan200.computercraft.api.ComputerCraftAPI.registerPeripheralProvider(new TileEntityComputerPort());
-	}
-}
